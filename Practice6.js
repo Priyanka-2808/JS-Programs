4aea64e68b1ff807a9f091a85be00b1a50b26350
iMac <imac@192.168.1.62>
2023-03-25 11:21:43 +0530

    my second commit

--- Practice6.js
+++ Practice6.js
@@ -1,25 +1,25 @@
-//1. Anagram or not
+1. Anagram or not
 
-    // function Anagram(a, b) {
+    function Anagram(a, b) {
         
-    //     let len1 = a.length;
-    //     let len2 = b.length;
-    //     if(len1 !== len2){
-    //        console.log('Invalid Input');
-    //        return
-    //     }
-    //     let str1 = a.split('').sort().join('');
-    //     let str2 = b.split('').sort().join('');
-    //     if(str1 === str2){
-    //        console.log("True");
-    //     } else { 
-    //        console.log("False");
-    //     }
-    //  }
+        let len1 = a.length;
+        let len2 = b.length;
+        if(len1 !== len2){
+           console.log('Invalid Input');
+           return
+        }
+        let str1 = a.split('').sort().join('');
+        let str2 = b.split('').sort().join('');
+        if(str1 === str2){
+           console.log("True");
+        } else { 
+           console.log("False");
+        }
+     }
      
-    //  Anagram("fried","fired")
+     Anagram("fried","fired")
 
-    //  2. pascal shape 
+2. pascal shape 
 
 let n = 5;
 let string = " ";
@@ -38,45 +38,45 @@ for (let i = 1; i <= n - 1; i++) {
   }
 console.log(string);
 
-// 3. Adding up of string
+3. Adding up of string
 
-// let arr=[1,2,3,4,6,7,8,9];
-// function Add(arr,final) {
-//     let hash = {};
-//     let sum=[]
-//     arr.forEach((item)=> {
-//         let diff = final - item;
-//         if(hash[diff.toString()]!== undefined) {
-//             sum.push([item,diff]);
-//         }
-//         hash[item.toString()]=item;
-//     })
-//     return sum;
-// }
-// console.log(Add(arr,9));
+let arr=[1,2,3,4,6,7,8,9];
+function Add(arr,final) {
+    let hash = {};
+    let sum=[]
+    arr.forEach((item)=> {
+        let diff = final - item;
+        if(hash[diff.toString()]!== undefined) {
+            sum.push([item,diff]);
+        }
+        hash[item.toString()]=item;
+    })
+    return sum;
+}
+console.log(Add(arr,9));
 
-// 4. Reverse the statement
-// function reverseWords(str) {
+4. Reverse the statement
+function reverseWords(str) {
 
-//     const words = str.split(' ');
+    const words = str.split(' ');
     
-//     const reversedWords = words.reverse();
+    const reversedWords = words.reverse();
     
-//     const reversedString = reversedWords.join(' ');
+    const reversedString = reversedWords.join(' ');
   
-//     return reversedString;
+    return reversedString;
     
-// }
-// const inputString = 'The quick brown fox jumps over the lazy dog.';
-// const outputString = reverseWords(inputString);
-// console.log(outputString);
+}
+const inputString = 'The quick brown fox jumps over the lazy dog.';
+const outputString = reverseWords(inputString);
+console.log(outputString);
 
 
-//   function reverseWords(str) {
+function reverseWords(str) {
 
-//     let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
-//     return reverseWordArr.join(" ");
-//   }
-//   console.log();
+    let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
+    return reverseWordArr.join(" ");
+  }
+  console.log();
 
 
