3557421f58edf025b3df518d7f59dfbfba5ea146
iMac <imac@192.168.1.62>
2023-03-25 14:15:24 +0530

    my second commit

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+const bubbleSort = (arr) => {
+    const n = arr.length;
+    
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          // Swap arr[j] and arr[j + 1]
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    
+    return arr;
+  };
+  
+  const arr = [8, 7, 1, 5, -2];
+  console.log(bubbleSort(arr)); 
+  
---
+++ infiniteloop.js
@@ -0,0 +1,8 @@
+let main;
+{  
+   for(;;)  
+   {  
+     console.log("Hello javatpoint");  
+   }  
+return 0;  
+} 
