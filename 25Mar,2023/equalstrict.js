97bb85af3a0ea3212731d63a4470dba0125aa11d
iMac <imac@192.168.1.88>
2023-03-27 16:36:29 +0530

    my third commit

---
+++ 25Mar,2023/equalstrict.js
@@ -0,0 +1,15 @@
+let num = 5;
+let str = "5";
+
+if (num == str) {
+  console.log("num == str");
+} else {
+  console.log("num != str");
+}
+
+if (num === str) {
+  console.log("num === str");
+} else {
+  console.log("num !== str");
+}
+
--- alphabet.js
+++ alphabet.js
@@ -3,22 +3,8 @@ let res = "The quick brown fox jumps over the lazy dog"
 function alphabet(str){
     let alpha = new Set("abcdefghijklmnopqrstuvwxyz")
     for (let a of str.toLowerCase()){
-        
         if (alpha.size == 0) return true
     }
     return false
 }
-// console.log(alphabet(str));
 console.log(alphabet(res));
-
-
-// function Alphabet(a) {
-//     var a = thewryuiop;
-//     var string=abcdefghijklmnopqrstuvwxyz;
-//     if(a.value.match(string))
-//     {
-//         console.log("true");
-//     } else {
-//         console.log("false");
-//     }
-// }
